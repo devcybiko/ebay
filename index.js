c4272f997b008d75db8945fc872f3f76f56f0258
Greg Smith <thinkable@thinkable.us>
2020-02-28 21:40:01 -0500

    automatic commit

--- index.js
+++ index.js
@@ -3,47 +3,92 @@ const axios = require('axios');
 const files = require('glstools').files;
 
 const authToken = files.read('./ebaytoken.txt').trim();
-const url1 = `https://api.ebay.com/commerce/taxonomy/v1_beta/category_tree/0`;
 const url2 = `https://api.ebay.com/commerce/taxonomy/v1_beta/get_default_category_tree_id?marketplace_id=EBAY_US`;
 const url3 = `https://api.ebay.com/buy/browse/v1/item_summary/search?filter=priceCurrency:USD`;
 const url4 = `https://api.ebay.com/buy/browse/v1/item/get_items_by_item_group?item_group_id=184057411986`;
-const tableCategory = `58058`;
+const url5 = `https://api.ebay.com/commerce/taxonomy/v1_beta/category_tree/0/get_category_subtree?category_id=58058`;
+const tabletCategory = `58058`;
 
-const request = {
-    method: 'get',
-    url: url1,
-    headers: {
-        Authorization: `Bearer ${authToken}`,
-    },
-
-    // `params` are the URL parameters to be sent with the request
-    // Must be a plain object or a URLSearchParams object
-    params: {
-        category_ids: tableCategory,
-        q: `ipad`,
-        // filter: `price:[200..500]`, 
-        limit: `100`,
-
-    },
-
-    timeout: 1000, // default is `0` (no timeout)
-    responseType: 'json', // default
-    responseEncoding: 'utf8', // default
-    //maxContentLength: 20000,
-};
-const instance = axios.create(request);
-instance.request(request)
-    .then(response => {
-        console.log(JSON.stringify(response.data, null, 2));
-        response.data.items.forEach(item => {
-            console.log(item.title);
-            console.log(`${item.price.value} ${item.condition} ${item.color} ${item.localizedAspects[0].value}`);
-        });
-        console.log("SUCCESS");
-    })
-    .catch(error => {
+async function ebayCategoryTree() {
+    const url = `https://api.ebay.com/commerce/taxonomy/v1_beta/category_tree/0`;
+    const request = {
+        method: 'get',
+        url: url,
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        }
+    }
+    try {
+        const instance = axios.create(request);
+        const response = await instance.request(request);
+        return response.data;
+    } catch (error) {
         console.log(error);
-        // console.log(error.config);
-        // console.log(error.response);
-        console.log("ERROR");
-    });
+    }
+}
+
+async function ebayCategoryTree() {
+    const url = `https://api.ebay.com/commerce/taxonomy/v1_beta/category_tree/0`;
+    const request = {
+        method: 'get',
+        url: url,
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        }
+    }
+    try {
+        const instance = axios.create(request);
+        const response = await instance.request(request);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function ebay() {
+    const request = {
+        method: 'get',
+        url: url5,
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+
+        // `params` are the URL parameters to be sent with the request
+        // Must be a plain object or a URLSearchParams object
+        params: {
+            category_ids: tabletCategory,
+            q: `ipad`,
+            // filter: `price:[200..500]`, 
+            limit: `100`,
+
+        },
+
+        timeout: 1000, // default is `0` (no timeout)
+        responseType: 'json', // default
+        responseEncoding: 'utf8', // default
+        //maxContentLength: 20000,
+    };
+    const instance = axios.create(request);
+    instance.request(request)
+        .then(response => {
+            console.log(JSON.stringify(response.data, null, 2));
+            response.data.items.forEach(item => {
+                console.log(item.title);
+                console.log(`${item.price.value} ${item.condition} ${item.color} ${item.localizedAspects[0].value}`);
+            });
+            console.log("SUCCESS");
+        })
+        .catch(error => {
+            console.log(error);
+            // console.log(error.config);
+            // console.log(error.response);
+            console.log("ERROR");
+        });
+}
+
+async function main() {
+    let response = await ebayCategoryTree();
+    console.log(JSON.stringify(response, null, 2));
+}
+
+main();
