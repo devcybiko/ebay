b04fbc55eda4afefa59dbf5a6024e0c605b7e3e5
Greg Smith <thinkable@thinkable.us>
2020-02-28 21:10:01 -0500

    automatic commit

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const axios = require('axios');
 const files = require('glstools').files;
 
 const authToken = files.read('./ebaytoken.txt').trim();
-const url = `https://api.ebay.com/commerce/taxonomy/v1_beta/get_default_category_tree_id?marketplace_id=EBAY_US`;
-const otherUrl = `https://api.ebay.com/buy/browse/v1/item_summary/search?filter=priceCurrency:USD`;
+const url = `https://api.ebay.com/commerce/taxonomy/v1_beta/category_tree/0`;
+const url2 = `https://api.ebay.com/commerce/taxonomy/v1_beta/get_default_category_tree_id?marketplace_id=EBAY_US`;
+const url3 = `https://api.ebay.com/buy/browse/v1/item_summary/search?filter=priceCurrency:USD`;
 const request = {
     method: 'get',
-    url: url,
+    url: url3,
     headers: {
         Authorization: `Bearer ${authToken}`,
     },
@@ -15,21 +16,22 @@ const request = {
     // `params` are the URL parameters to be sent with the request
     // Must be a plain object or a URLSearchParams object
     params: {
-        category_ids: `108765`,
-        q: `Beatles`,
-        filter: `price:[200..500]`, 
-        limit: `10`
+        category_ids: `58058`,
+        q: `ipad`,
+        // filter: `price:[200..500]`, 
+        // limit: `10`
+
     },
 
     timeout: 1000, // default is `0` (no timeout)
     responseType: 'json', // default
     responseEncoding: 'utf8', // default
-    maxContentLength: 20000,
+    //maxContentLength: 20000,
 };
 const instance = axios.create(request);
 instance.request(request)
     .then(response => {
-        console.log(response.data);
+        console.log(JSON.stringify(response.data, null, 2));
         console.log("SUCCESS");
     })
     .catch(error => {
